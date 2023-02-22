dedb8110df71260a6a837cc5ca8d5f14b21cd6e7
AmazingTonmoy <amazingtonmoy@gmail.com>
2023-02-23 05:06:03 +0600

    changed some feature

--- js/dice.js
+++ js/dice.js
@@ -1,19 +1,30 @@
-var randomNumber1 = Math.floor(Math.random() * 6)+1;
-var randomNumber2 = Math.floor(Math.random() * 6)+1;
- 
+document.getElementById('btn-game').addEventListener('click',function(){
 
-let randomNum1 = document.getElementById("img-1").setAttribute('src',`images/dice${randomNumber1}.png`);
-let randomNum2 = document.getElementById("img-2").setAttribute('src',`images/dice${randomNumber2}.png`);
+    var randomNumber1 = Math.floor(Math.random() * 6)+1;
+    var randomNumber2 = Math.floor(Math.random() * 6)+1;
+     
+    
+    let randomNum1 = document.getElementById("img-1").setAttribute('src',`images/dice${randomNumber1}.png`);
+    let randomNum2 = document.getElementById("img-2").setAttribute('src',`images/dice${randomNumber2}.png`);
+    
+    
+    let heading = document.getElementById('heading');
+    
+    if (randomNumber1 === randomNumber2){
+        heading.innerText = "🏁 Draw 🏁"
+    }
+    else if(randomNumber1 > randomNumber2 ){
+        heading.innerText = "🚩 Player 1 Win"
+    }
+    else{
+        heading.innerText = "Player 2 Win 🚩"
+    }
+    
+    const cardId = document.getElementById('btn-game');
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  cardId.style.backgroundColor = randomColor;
+  
+    
+})
 
 
-let heading = document.getElementById('heading');
-
-if (randomNumber1 === randomNumber2){
-    heading.innerText = "🏁 Draw 🏁"
-}
-else if(randomNumber1 > randomNumber2 ){
-    heading.innerText = "🚩 Player 1 Win"
-}
-else{
-    heading.innerText = "Player 2 Win 🚩"
-}
