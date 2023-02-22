e74802c8dd96e62d939368e7b35d8fc6b6936431
AmazingTonmoy <amazingtonmoy@gmail.com>
2023-02-23 04:41:45 +0600

    dice game complete

---
+++ js/dice.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6)+1;
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+ 
+
+let randomNum1 = document.getElementById("img-1").setAttribute('src',`images/dice${randomNumber1}.png`);
+let randomNum2 = document.getElementById("img-2").setAttribute('src',`images/dice${randomNumber2}.png`);
+
+
+let heading = document.getElementById('heading');
+
+if (randomNumber1 === randomNumber2){
+    heading.innerText = "🏁 Draw 🏁"
+}
+else if(randomNumber1 > randomNumber2 ){
+    heading.innerText = "🚩 Player 1 Win"
+}
+else{
+    heading.innerText = "Player 2 Win 🚩"
+}
